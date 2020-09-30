c0830cd2bb944c468ac85996206c1a11095a3e69
Anup <10anupkalita@gmail.com>
2020-09-30 20:42:02 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// Smooth Scrolling
+$('.nav-bar a, .button a').on('click', function(event){
+    if((this.hash!=='')){
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top -10
+      },
+      700
+      );
+    }
+  });
